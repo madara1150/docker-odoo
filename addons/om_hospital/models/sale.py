83eb95c51ea09471fb93a3ab450f1bc62daeda3c
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2024-11-06 10:39:02 +0700

    add docker

---
+++ addons/om_hospital/models/sale.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models, tools
+
+# from odoo.addons.sale.models.sale import SaleOrder as OdooSaleOrder
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    sale_description = fields.Char(string="Sale Description")
